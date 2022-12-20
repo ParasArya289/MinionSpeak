5885a74c0eb9317caa2208266330c02401d30951
ParasArya289 <parasarya289@gmail.com>
2022-12-20 12:34:32 +0530

    style: added new background and changed font color in textarea

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var apiURL = "https://api.funtranslations.com/translate/minion.json"
+var apiURL = "https://api.funtranslations.com/translate/minion.json/";
 
 function getTranslationURL(input) {
-    return apiURL + "?" + "text=" + input
+  return apiURL + "?" + "text=" + input;
 }
 
 function errorHandler(err) {
-    console.log("Error", err);
-    alert("something wrong with server! try again after some time")
+  console.log("Error", err);
+  alert("something wrong with server! try again after some time");
 }
 
-
 function clickHandler() {
-    var inputText = txtInput.value; // taking input
+  var inputText = txtInput.value; // taking input
 
-    // calling server for processing
-    fetch(getTranslationURL(inputText))
-        .then((response )=> {
-            if(!response.ok){
-                throw new Error("something went wrong");
-            }
-        })
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
-           })
-        .catch(err => alert(err.message));
-};
+  // calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
+      return response.json();
+    })
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; // output
+    })
+    .catch((err) => alert(err.message));
+}
 
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
